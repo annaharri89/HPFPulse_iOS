8e722417c3fab5510b597a6e44114b2b5e5c4b20
Anna Harrison <anna@Macminis-Mac-mini.local>
2018-05-17 10:57:43 -0400

    First commit

---
+++ Pulse_iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pulse_iOS
+//
+//  Created by Anna Harrison on 5/17/18.
+//  Copyright © 2018 The Humanity Preservation Foundation. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pulse_iOSTests/Pulse_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pulse_iOSTests.swift
+//  Pulse_iOSTests
+//
+//  Created by Anna Harrison on 5/17/18.
+//  Copyright © 2018 The Humanity Preservation Foundation. All rights reserved.
+//
+
+import XCTest
+@testable import Pulse_iOS
+
+class Pulse_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
